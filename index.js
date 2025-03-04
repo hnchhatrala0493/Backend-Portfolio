29c42af1bfdfa63b4029c2c802464afd29479e2c
Himanshu <hnchhatrala@gmail.com>
2025-03-04 18:10:08 +0530

    file undo

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-import axios from 'axios'
-import router from './router' // Import your router instance
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-axios.defaults.baseURL = 'https://backend-portfolio-pw1y.vercel.app'
+app.use(cors())
+app.use(express.json())
 
-// Intercept responses
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      router.push('/login') // Redirect to login page
-    }
-    return Promise.reject(error)
-  },
-)
+app.get('/', (req, res) => {
+  res.send('Backend is running on Vercel!')
+})
 
-export default axios
+// Export a function instead of running `app.listen`
+module.exports = (req, res) => {
+  return app(req, res)
+}
