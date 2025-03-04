a9f234148f23e161954e18d6246be27edf361d14
Himanshu <hnchhatrala@gmail.com>
2025-03-04 18:02:20 +0530

    updated index file

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+import axios from 'axios'
+import router from './router' // Import your router instance
 
-app.use(cors())
-app.use(express.json())
+axios.defaults.baseURL = 'https://backend-portfolio-pw1y.vercel.app'
 
-app.get('/', (req, res) => {
-  res.send('Backend is running on Vercel!')
-})
+// Intercept responses
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      router.push('/login') // Redirect to login page
+    }
+    return Promise.reject(error)
+  },
+)
 
-// Export a function instead of running `app.listen`
-module.exports = (req, res) => {
-  return app(req, res)
-}
+export default axios
