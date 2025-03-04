38d8b30ff625e5a54183cc6309d0f0e661495d93
Himanshu <hnchhatrala@gmail.com>
2025-03-04 18:17:35 +0530

    file updated

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import doConnection from './config/dbConnection.js'
+import Skills from './routes/skills.js'
+import Education from './routes/education.js'
 
-app.use(cors())
+dotenv.config()
+const app = express()
+
+app.use(cors({ origin: '*' }))
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+app.use('/uploads', express.static('uploads'))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Backend is running on Vercel!')
-})
+app.use('/api/skills', Skills)
+app.use('/api/education', Education)
 
-// Export a function instead of running `app.listen`
-module.exports = (req, res) => {
-  return app(req, res)
-}
+doConnection()
+app.listen(5000, (error) => {
+  if (!error) console.log('Server is Successfully Running,  and App is listening on port ' + 5000)
+  else console.log("Error occurred, server can't start", error)
+})
