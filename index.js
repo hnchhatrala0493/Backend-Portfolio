e95909ec404ed07677fce3a883a8aa26eecd3109
Himanshu <hnchhatrala@gmail.com>
2025-03-04 17:55:02 +0530

    updated File

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-import { v2 as cloudinary } from 'cloudinary'
-import dotenv from 'dotenv'
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-dotenv.config() // ✅ Load environment variables
-
-// ✅ Correct Configuration
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+app.use(cors())
+app.use(express.json())
+app.use('/api/skills', Skills)
+app.use('/api/education', Education)
+app.get('/', (req, res) => {
+  res.send('Backend is running on Vercel!')
 })
-;(async function () {
-  try {
-    // ✅ Upload an image
-    const uploadResult = await cloudinary.uploader.upload()
 
-    console.log('Upload Result:', uploadResult)
-
-    // ✅ Optimize delivery
-    const optimizeUrl = cloudinary.url('shoes', {
-      fetch_format: 'auto',
-      quality: 'auto',
-    })
-
-    console.log('Optimized URL:', optimizeUrl)
-
-    // ✅ Transform the image: auto-crop
-    const autoCropUrl = cloudinary.url('shoes', {
-      crop: 'auto',
-      gravity: 'auto',
-      width: 500,
-      height: 500,
-    })
-
-    console.log('Auto-Cropped URL:', autoCropUrl)
-  } catch (error) {
-    console.error('Error:', error)
-  }
-})()
+// Export a function instead of running `app.listen`
+module.exports = (req, res) => {
+  return app(req, res)
+}
